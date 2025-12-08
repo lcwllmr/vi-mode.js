65d9846f8e53f2206de2e5b96f24afd612856b7c
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-12-08 16:53:34 +0100

    feat(demo): show package version

--- demo/main.ts
+++ demo/main.ts
@@ -4,6 +4,9 @@ let controller: ViModeController;
 
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
+    const versionSpan = document.getElementById("version") as HTMLSpanElement;
+    versionSpan.textContent = "v" + __VERSION__;
+
     const editorDiv = document.getElementById("editor") as HTMLDivElement;
     controller = new ViModeController(
       editorDiv,
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
+import pkg from "../package.json";
 
 export default defineConfig({
   base: "./",
   server: { open: true },
+  define: {
+    __VERSION__: JSON.stringify(pkg.version),
+  },
 });
